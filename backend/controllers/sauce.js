e9220b90e82fc33b13fc83e98cbbf97e98706468
Alexandre Audigié <alexandre.audigie@orange.fr>
2022-06-02 17:56:21 +0200

    old img deleted in put route

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -70,9 +70,20 @@ exports.getOneSauce = (req, res, next) => {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
       } : { ...req.body };
-    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
-      .catch(error => res.status(400).json({ error }));
+        if (req.file){
+          console.log("il y a une nouvelle image à ajouter");
+          const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+              Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
+            .catch(error => res.status(400).json({ error }));
+          });
+        } else {
+          console.log("il n'y a pas de nouvelle image à ajouter");
+          Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
+        .catch(error => res.status(400).json({ error }));
+        }
       }
     )
   };
